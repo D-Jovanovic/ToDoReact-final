74e0c4d5e3965e5e54fe2a3ff52f12988a4b246b
D-Jovanovic <d.jovanovic.it@gmail.com>
2022-04-14 13:20:06 +0100

    dodavanje stavke

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  /*
   const [inputValue, setInputValue] = useState("");
-  */
+
   const [todo, setTodo] = useState([
     { title: "jabuka" },
     { title: "kruska" },
@@ -21,8 +20,29 @@ function App() {
 
   const [filter, setFilter] = useState("");
 
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setTodo([...todo, {title: inputValue}]);
+    setInputValue("");
+}
+
   return (
     <div>
+      <form onSubmit={formSubmit}>
+        <input
+          type="text"
+          placeholder="Add..."
+          className="#"
+          value={inputValue}
+          required
+          onChange={(event) => {
+            setInputValue(event.target.value);
+          }}
+        />
+        <button className="#" type="submit">
+          Add
+        </button>
+      </form>
       <input
         type="text"
         placeholder="Search..."
@@ -30,20 +50,21 @@ function App() {
           setFilter(event.target.value);
         }}
       />
-      {todo.filter((item) => {
-        if (filter === ""){
-          return item
-        } else if (item.title.toLowerCase().includes(filter.toLowerCase())){
-          return item
-        }
-      })
-      .map((item, key) => {
-        return (
-          <div key={key}>
-            <li>{item.title}</li>
-          </div>
-        );
-      })}
+      {todo
+        .filter((item) => {
+          if (filter === "") {
+            return item;
+          } else if (item.title.toLowerCase().includes(filter.toLowerCase())) {
+            return item;
+          }
+        })
+        .map((item, key) => {
+          return (
+            <div key={key}>
+              <li>{item.title}</li>
+            </div>
+          );
+        })}
     </div>
   );
 }
