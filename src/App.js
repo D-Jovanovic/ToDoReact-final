5abc987eff1779b9b08ff094c42817cf67c12f39
D-Jovanovic <d.jovanovic.it@gmail.com>
2022-04-14 12:36:10 +0100

    filter search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  /*
+  const [inputValue, setInputValue] = useState("");
+  */
+  const [todo, setTodo] = useState([
+    { title: "jabuka" },
+    { title: "kruska" },
+    { title: "banana" },
+    { title: "sljiva" },
+    { title: "pomorandza" },
+    { title: "breskva" },
+    { title: "kajsija" },
+    { title: "orah" },
+    { title: "badem" },
+    { title: "avokado" },
+    { title: "mandarina" },
+  ]);
+
+  const [filter, setFilter] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setFilter(event.target.value);
+        }}
+      />
+      {todo.filter((item) => {
+        if (filter === ""){
+          return item
+        } else if (item.title.toLowerCase().includes(filter.toLowerCase())){
+          return item
+        }
+      })
+      .map((item, key) => {
+        return (
+          <div key={key}>
+            <li>{item.title}</li>
+          </div>
+        );
+      })}
     </div>
   );
 }
